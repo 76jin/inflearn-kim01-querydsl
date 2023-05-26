e0e91332f31133d72bad851567f16389e1aa7e1d
76jin <ranian129@gmail.com>
2023-05-26 14:29:53 +0900

    실무활용 - 순수 JPA 리포지토리와 Querydsl
    - JPAQueryFactory new 객체 생성 방법(테스트 편리)
    - JPAQueryFactory 빈으로 등록 방법(빈 주입 편리)

---
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,56 @@
+package study.querydsl.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    private EntityManager em;
+    
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void basicTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member foundMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(foundMember).isEqualTo(member);
+
+        List<Member> result1 = memberJpaRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername(member.getUsername());
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    void queryDslTest() {
+        Member member = new Member("member2", 20);
+        memberJpaRepository.save(member);
+
+        Member foundMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(foundMember).isEqualTo(member);
+
+        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl(member.getUsername());
+        assertThat(result2).containsExactly(member);
+    }
+}
